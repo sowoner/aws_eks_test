d2e48196cb7774cbc604092a4e5471e34a20c839
sowoner <botflow@naver.com>
2023-10-17 16:10:33 +0900

    EKS Initial Commit

---
+++ modules/eks_cluster/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "VPC ID Output"
+}
+
+output "private_subnets_cidr_blocks" {
+  value       = module.vpc.private_subnets_cidr_blocks
+  description = "Private_Subnets_Cidr_Blocks Output"
+}
+
+output "database_subnets" {
+  value       = module.vpc.database_subnets
+  description = "Database_Subnets Output"
+}
+
+output "database_subnet_group" {
+  value       = module.vpc.database_subnet_group
+  description = "Database_Subnet_Group Output"
+}
